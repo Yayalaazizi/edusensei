d30826d7d567facc7b6ea516794f944765c43524
Bahij <ahmedbahij0@gmail.com>
2026-04-27 12:52:27 +0100

    Setuping the project from scratch (deleted some files, modified some..)

---
+++ frontend/src/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div style={{ fontSize: 40, color: 'blue', textAlign: 'center', marginTop: 100 }}>
+      Hello EduSensei 👋
+    </div>
+  )
+}
+
+export default Test
